cc31a70cf6ad2c1134b885419d0f53e4a64bc6dd
lanseria <zhangchao564265135@hotmail.com>
2022-01-24 15:32:15 +0800

    add naive-ui

--- src/render/components/HelloWorld.vue
+++ src/render/components/HelloWorld.vue
@@ -3,8 +3,12 @@
 
   <textarea cols="60" rows="10" v-model="log" disabled />
   <div style="margin-top: 20px">
-    <input type="text" v-model="msg" placeholder="send msg to main process" />
-    <button style="margin-left: 20px" @click="sendMsg">Send</button>
+    <n-input
+      style="width: 200px"
+      v-model:value="msg"
+      placeholder="send msg to main process"
+    ></n-input>
+    <n-button style="margin-left: 20px" @click="sendMsg">Send</n-button>
   </div>
 </template>
 
@@ -13,7 +17,7 @@ import { EVENTS } from "@common/events";
 import { sendMsgToMainProcess } from "@render/api";
 import { useIpc } from "@render/plugins/ipc";
 import { ref } from "vue";
-
+import { NInput, NButton } from "naive-ui";
 const props = defineProps({
   title: {
     type: String,
@@ -26,6 +30,7 @@ const msg = ref("");
 
 const sendMsg = async () => {
   try {
+    console.log(msg.value);
     log.value += `[render]: ${msg.value} \n`;
     const { data } = await sendMsgToMainProcess(msg.value);
     log.value += `[main]: ${data}  \n`;
