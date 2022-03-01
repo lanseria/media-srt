c169bb21c9a2f81897295f4e12267f263f77f84f
lanseria <zhangchao564265135@hotmail.com>
2022-03-01 22:10:30 +0800

    fix electron-build config

---
+++ electron-builder.github.config.js
@@ -0,0 +1,21 @@
+/**
+ * @type {import('electron-builder').Configuration}
+ * @see https://www.electron.build/configuration/configuration
+ */
+const config = {
+  directories: {
+    output: "dist/electron",
+  },
+  npmRebuild: false,
+  buildDependenciesFromSource: true,
+  files: ["dist/main/**/*", "dist/render/**/*"],
+  nsis: {
+    oneClick: false,
+    allowElevation: true,
+    allowToChangeInstallationDirectory: true,
+    createDesktopShortcut: true,
+    createStartMenuShortcut: true,
+  },
+};
+
+module.exports = config;
